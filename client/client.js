fd0211b7498caa2b7516f7de242ffb6c9f5022e7
Ismail Ghelle <ismailghelle6@gmail.com>
2019-06-02 00:56:41 -0400

    updates

--- client/client.js
+++ client/client.js
@@ -1,5 +1,9 @@
 
 const form = document.querySelector('form');
+const loadingElement = document.querySelector('.loading');
+const API_URL = 'http://localhost:5000/tweets';
+
+loadingElement.style.display = 'none';
 
 form.addEventListener('submit', function(event){
     event.preventDefault();
@@ -12,6 +16,22 @@ form.addEventListener('submit', function(event){
         name,
         content,
     };
-
     console.log(tweet);
+    form.style.display = 'none';
+    loadingElement.style.display = ''; 
+
+    fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify(tweet),
+        headers: {
+           'content-type': 'application/json',
+           'Accept': 'application/json'
+        }
+    }).then(response => response.json())
+        .then(createdTweet => {
+            console.log(createdTweet);
+            form.reset();
+            form.style.display = '';
+            loadingElement.style.display = none; 
+    });
 });
