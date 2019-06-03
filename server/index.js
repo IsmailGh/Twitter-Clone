a8a98a18889b835c534e84a34917be13900fe5f7
Ismail Ghelle <ismailghelle6@gmail.com>
2019-06-03 03:11:55 -0400

    server updates

--- server/index.js
+++ server/index.js
@@ -1,25 +1,27 @@
+/*jshint esversion: 6 */
+
 const express = require('express');
 const cors = require('cors');
 const monk = require('monk');
 
 const app = express();
 
-const db = monk('localhost/tweets');
+const db = monk(process.env.MONGO_URI || 'localhost/twitter');
 const tweets = db.get('tweets');
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-app.get('/', function(req,res){
+app.get('/', (req,res) =>{
     res.json({
         message: 'jhellop' 
     }); 
 });
 
-app.get('/tweets', function(req,res){
+app.get('/tweets', (req,res) =>{
     tweets  
         .find()
-        then(tweets => {
+        .then(tweets => {
             res.json(tweets);
         });
 });
@@ -29,14 +31,14 @@ function isValidTweet(tweet){
         tweet.content && tweet.content.toString().trim() != '';
 }
 
-app.post('/tweets', function(req,res){
+app.post('/tweets', (req,res) =>{
     if(isValidTweet(req.body)){
-
         const tweet = {
             name: req.body.name.toString(),
             content: req.body.content.toString(),
             created: new Date()
         };
+        console.log(tweet);
         tweets 
             .insert(tweet)
             .then(createdTweet =>{
